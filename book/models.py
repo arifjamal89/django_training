f25b73329c24918b28251f34757314ce4e4d41da
arifjamal89 <arifjamal89@gmail.com>
2018-12-20 16:56:29 +0800

    Second push sendiri punya

--- book/models.py
+++ book/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
+from django.utils.timezone import now # import time now
 # Create your models here.
 
 
 class Book(models.Model):
-    ...
+    title=models.CharField(max_length=100)
+    published_date=models.DateField(default=now)
+    author=models.CharField(max_length=100)
+    created_date=models.DateTimeField(auto_now=True) #ini adalah untuk insert auto data create date
+
+    def __str__(self):
+        return self.title
+
+
 
--- django_training/urls.py
+++ django_training/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from book.views import book_list #wajib import dari views.py untuk tunjuk dekat url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('book/',book_list,name='book_list')
+
+    #path('book/',BookView.as_view())
 ]
+
